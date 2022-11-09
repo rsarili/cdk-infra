537a1221b9d827885cbbfa4b5ab0eac4043f3abb
rifatsarili <rifat.sarili@nordcloud.com>
2022-11-09 10:04:04 +0100

    add vpc

--- lib/cdk-infra-stack.ts
+++ lib/cdk-infra-stack.ts
@@ -1,16 +1,12 @@
 import * as cdk from 'aws-cdk-lib';
 import { Construct } from 'constructs';
-// import * as sqs from 'aws-cdk-lib/aws-sqs';
+import * as ec2 from 'aws-cdk-lib/aws-ec2'
 
 export class CdkInfraStack extends cdk.Stack {
   constructor(scope: Construct, id: string, props?: cdk.StackProps) {
     super(scope, id, props);
 
-    // The code that defines your stack goes here
-
-    // example resource
-    // const queue = new sqs.Queue(this, 'CdkInfraQueue', {
-    //   visibilityTimeout: cdk.Duration.seconds(300)
-    // });
+    const vpc = new ec2.Vpc(this, 'my-cdk-vpc');
+   
   }
 }
