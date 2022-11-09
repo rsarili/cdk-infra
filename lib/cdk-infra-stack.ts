ec2bd84dfb209800504671e2a09446184c86016d
rifatsarili <rifat.sarili@nordcloud.com>
2022-11-09 14:10:08 +0100

    use only one az

--- lib/cdk-infra-stack.ts
+++ lib/cdk-infra-stack.ts
@@ -6,7 +6,21 @@ export class CdkInfraStack extends cdk.Stack {
   constructor(scope: Construct, id: string, props?: cdk.StackProps) {
     super(scope, id, props);
 
-    const vpc = new ec2.Vpc(this, 'test-vpc');
+    const vpc = new ec2.Vpc(this, 'test-vpc', {
+      subnetConfiguration: [
+        {
+          cidrMask: 24,
+          name:'public',
+          subnetType: ec2.SubnetType.PUBLIC
+        },
+        {
+          cidrMask: 24,
+          name:'private',
+          subnetType: ec2.SubnetType.PRIVATE_WITH_EGRESS
+        }
+      ],
+      maxAzs: 1
+    });
    
   }
 }
